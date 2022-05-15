390af6ddf9616aaeb430089dcf67c142ea571a67
Quasimo <linuximo@gmail.com>
2022-05-16 01:12:43 +0800

    simple data

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,37 @@
 import Head from 'next/head'
 
+const site = {
+  title: 'Lin Zhizhao',
+  subTitle: '隐藏人物睡不着',
+  description: '日常互联网发现，随缘摄影分享',
+  email: 'quasimo@qq.com',
+};
+
+function mailTo(site) {
+  return 'mailto:' + site.email
+}
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>{site.subTitle}_{site.title}_{site.description}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        {site.title}
         </h1>
+        <h2>{site.subTitle}</h2>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          {site.description}
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href={mailTo(site)} className="card">
+            {site.email}
           </a>
         </div>
       </main>
